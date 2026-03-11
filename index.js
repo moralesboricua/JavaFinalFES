65f75576d8da1cded7ee6c44a8a474124d6d3c05
moralesboricua <moralesboricua@gmail.com>
2026-03-11 16:29:51 -0400

    updated api

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+// https://ghibli-api.vercel.app/api/films  http://www.omdbapi.com/?i=tt3896198&apikey=31123595//
+
+async function main() {
+    const movies = await fetch("https://ghibli-api.vercel.app/api/films");
+    const moviesData = await movies.json();
+    console.log(
+    moviesData.map( 
+        (movies) => `<div class="movie-card">
+        <div class="movie-card__container">
+            <h3>Title</h3>
+            <p><b>Release Date: </b> fghjklkjhgfd</p>
+            <p><b>Description: </b> dfghjkjhgfd</p>
+        </div>
+    </div>`
+        )
+        );
+    
+    }
+
+main();
+
+
+
